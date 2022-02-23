926dedbf31c8dd781dedc479dae7aeb0c7cd6aca
Alexandra Zinkevich <alexand18a@gmail.com>
2022-02-23 21:16:19 +0300

    ,

--- lab_1/main/main/main.cpp
+++ lab_1/main/main/main.cpp
@@ -9,7 +9,7 @@ int main()
     Vector vec1(a * b, 13);
     Vector vec2(createNumber(-7), ONE);
     Vector vec3;
-    vec3 = (vec1 + vec2);
+    vec3 = vec1 + vec2;
     Number fi = vec3.angle_coordinate();
     Number r = vec3.radius_coordinate();
     std::cout << "radius = " << r.getNumber() << "\n" << "angle = " << fi.getNumber() << "\n";
