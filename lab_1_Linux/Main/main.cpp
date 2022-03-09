1ccc8610a6baa0903aa18cdf4f4170a3b06638b9
alexand18a <99791211+alexand18a@users.noreply.github.com>
2022-03-09 18:45:15 +0300

    Add files via upload

---
+++ lab_1_Linux/Main/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Number.h"
+#include "Vector.h"
+
+int main()
+{
+    Number a(3);
+    Number b(7);
+    Vector vec1(a * b, 13);
+    Vector vec2(createNumber(-7), ONE);
+    Vector vec3;
+    vec3 = vec1 + vec2;
+    Number fi = vec3.angle_coordinate();
+    Number r = vec3.radius_coordinate();
+    std::cout << "radius = " << r.getNumber() << "\n" << "angle = " << fi.getNumber() << "\n";
+}
